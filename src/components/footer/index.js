21678d4eb687b96c3d89050a4fabf0fd59b92fb5
abhisheksingh7455 <absingh7455@gmail.com>
2021-12-25 13:34:44 +0530

    check

---
+++ src/components/footer/index.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            Tried making with care (❁´◡`❁) by : {` `}
+            <a className="nav-link" href="https://github.com/jatinsajwan3841">
+                Jatin
+            </a>
+            <br></br>
+            Contributions by : {` `}
+            <a className="nav-link" href="https://www.linkedin.com/in/abhishek-singh-9244b8144/">
+                Abhishek Singh
+            </a>
+        </div>
+    )
+}
+
+export default Footer
