5731e2625250590089f59cc6b8c16fd34a08a2cf
abhisheksingh7455 <absingh7455@gmail.com>
2022-03-28 15:15:01 +0530

    5th Sem Result

--- src/components/constant/index.js
+++ src/components/constant/index.js
@@ -33,7 +33,8 @@ export const EXCELFILES = [
     'dat/B. TECH. I SEM DEC 19.xlsx',
     'dat/B. TECH. II SEM DECEMBER 2020(COVID).xlsx',
     'dat/B. TECH. III SEM DECEMBER 2020(COVID 19).xlsx',
-    'dat/B. TECH. IV SEM JUNE 2021(COVID 19).xlsx'
+    'dat/B. TECH. IV SEM JUNE 2021(COVID 19).xlsx',
+    'dat/Vth SEM.xlsx'
     
 
     
